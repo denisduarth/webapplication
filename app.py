d48e5c2b2278049f6d6a73afaeaa5184566bc018
Denis Duarth <denisduarth30@gmail.com>
2022-05-27 21:04:24 +0000

    Atualiza para uma aplicação web usando Flask

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+# Ao abrir o gitpod, execute:
+# pip install -r requirements.txt
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def daniel():
+    return 'Daniel Calvo'
+
+app.run()
+
 print('Daniel Calvo')
