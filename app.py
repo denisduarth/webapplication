0ad80b183356442aa6ab472f47f8c36054552c18
Denis Duarth <denisduarth30@gmail.com>
2022-06-10 21:19:09 +0000

    criamos add.html e index.html

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
 # Ao abrir o gitpod, execute:
 # pip install -r requirements.txt
 
-from flask import Flask
+from flask import Flask, render_template, request
+from uuid import uuid4
 
 app = Flask(__name__)
 
+lista_livros = [
+    {'id': uuid4(), 'autor': 'machado de assis', 'título': 'memórias póstumas de brás cubas', 'situação': 'lido'},
+    {'id': uuid4(), 'autor': 'stephen king', 'título': 'it, a coisa', 'situação': 'não lido'},
+    {'id': uuid4(), 'autor': 'edgar allan poe', 'título': 'o corvo', 'situação': 'não lido'},
+]
+
 @app.route('/')
-def daniel():
-    return 'Daniel Calvo'
+def index():
+    return render_template('index.html',lista_livros = lista_livros)
 
-app.run()
+@app.route('/add')
+def add():
+    return render_template('add.html')
 
-print('Daniel Calvo')
+@app.route('/save', methods=['POST'])
+def save():
+    autor = request.form['Autor']
+    titulo = request.form['Título']
+    situação = request.form['Situação']
+    lista_livros.append({'id': uuid4(),'autor': autor,'título': titulo,'situação': situação})
+    return render_template('index.html', lista_livros=lista_livros)
+
+@app.route('/update/<id>', methods=['POST'])
+def edit():
+    return add()
+
+@app.route('/delete/<id>')
+def delete():
+    del lista_livros['id']
+
+
+
+
+
+app.run(debug=True)
